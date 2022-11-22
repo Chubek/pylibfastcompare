70f95a5b4160f9a401a8f433562d6b37e354dcce
Chubak Bidpaa <chubakbidpaa@gmail.com>
2022-11-23 02:43:56 +0330

    added interleaving

--- mmapfastaparser.py
+++ mmapfastaparser.py
@@ -8,17 +8,29 @@ class MmapFastaParser:
             mmpf = mmap(fbr.fileno(), 0, prot=PROT_READ)
 
         self.mmp_iter = iter(mmpf.readline, b"")
+        self.curr_line = next(self.mmp_iter)
 
 
     def __next__(self) -> Tuple[str, str]:
         try:
-            header = next(self.mmp_iter).decode().strip()[1:]
+            header = self.curr_line.decode().strip()[1:]
             seq = next(self.mmp_iter).decode().strip()
+            
+            while True:
+                self.curr_line = next(self.mmp_iter)
 
-            return (header, seq)
+                if self.curr_line[0] == ord('>'):
+                    return (header, seq)
+                else:
+                    seq += self.curr_line.decode().strip()
+
         except:
             return None
 
 
     def __pos__(self) -> Tuple[str, str]:
         return self.__next__()
+
+
+aa = MmapFastaParser("EOG7B0GZN.aa.fa")
+print(next(aa))
