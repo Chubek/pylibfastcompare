2c74e67c6dc04fc24128c51877b500f7b0cc2787
Chubak Bidpaa <chubakbidpaa@gmail.com>
2022-12-04 22:16:53 +0330

    hashmap working

--- fastcompare.c
+++ fastcompare.c
@@ -26,161 +26,6 @@ const uint8_t lookup_num_diffs[257] = {
 };
 
 
-TSTNode *new_node(uint8_t data) {
-    TSTNode* temp = (TSTNode*) malloc(sizeof(TSTNode));
-    temp->data = data;
-    temp->is_end = 0;
-    temp->left = temp->eq = temp->right = NULL;
-    return temp;
-}
-
-void insert_tst(TSTNode **root, uint8_t *word) {
-    if (!(*root)) *root = new_node(*word);
-    if ((*word) < (*root)->data) insert_tst(&((*root)->left), word);
-    else if ((*word) > (*root)->data) insert_tst(&((*root)->right), word);
-    else {
-        if (*(word + 1)) insert_tst(&(*root)->eq, word + 1);
-        else (*root)->is_end = 1;
-    }
-}
-int search_tst(TSTNode *root, uint8_t *word) {
-    if (!root) return 0;
-    if (*word < (root)->data) return search_tst(root->left, word);
-    else if (*word > (root)->data) return search_tst(root->right, word);
-
-    else {
-        if (*(word + 1) == '\0') return root->is_end;
-
-        return search_tst(root->eq, word + 1);
-    }
-};
-void _traverse_tst(TSTNode *root, uint8_t *buffer, int depth) {
-    if (root) {
-        _traverse_tst(root->left, buffer, depth);
-
-        buffer[depth] = root->data;
-        if (root->is_end) {
-            buffer[depth + 1] = '\0';
-            return;
-        }
-
-        _traverse_tst(root->eq, buffer, depth + 1);
-        _traverse_tst(root->right, buffer, depth);
-    }
-}
-uint8_t *traverse_tst(TSTNode *root) {
-    uint8_t *buffer = malloc(BUFFER_MAX);
-    _traverse_tst(root, buffer, 0);
-
-    return buffer;
-}
-
-int find_common_prefix(TSTNode *root, uint8_t *word, int *uint8_tindex) {
-    if (!(*word) || root->is_end) return 1;
-    int res = 0;
-    uint8_t c = word[*uint8_tindex];
-    
-    if (c == root->data) {
-        ++(*uint8_tindex);
-        res = find_common_prefix(root->eq, word, uint8_tindex);
-        if (!res) return 1;    
-    }
-    else if (c < root->data) {
-        if (!root->left) {
-            return 0;
-        } else {
-            res = find_common_prefix(root->left, word, uint8_tindex);
-            if (!res) return 1;
-        }
-    } 
-    else if (c > root->data) {
-        if (!root->right) {
-            return 0;
-        } else {
-            res = find_common_prefix(root->right, word, uint8_tindex);
-            if (!res) return 1;
-        }
-    }
-
-    return 0;
-}
-
-TrieNode *get_node_trie() {
-    TrieNode *node = malloc(sizeof(TrieNode));
-    if (!node) {
-        printf("Error allocating trie node\n");
-        exit(1);
-    }
-
-    node->is_end = 0;
-    for (int i = 0; i < ALPHA_SIZE; ++i) {
-        node->children[i] = NULL;
-    }
-
-    return node;
-}
-
-void insert_trie(TrieNode *root, uint8_t *key, int stlen) {
-    int len = stlen;
-    int index = 0;
-    uint8_t c;
-    TrieNode *crawler = root;
-    for (int l = 0; l < len; ++l) {
-        c = key[l];
-        if (c == '\0') {
-            crawler->is_end = 1;
-            break;
-        }
-        index = CHAR_TO_IND(c);      
-        if (!crawler->children[index]) crawler->children[index] = get_node_trie();
-        crawler = crawler->children[index];
-    }    
-}
-int count_children_trie(TrieNode *root, int *index) {
-    int cnt = 0;
-
-    for (int i = 0; i < ALPHA_SIZE; ++i) {
-        if (root->children[i]) {
-            cnt++;
-            *index = i;
-        }
-    }
-
-    return cnt;
-}
-int walk_trie(TrieNode *root) {
-    TrieNode *crawler = root;
-    int size_common = 0;
-    int index;
-
-
-    while (count_children_trie(crawler, &index) == 1 && !crawler->is_end) {
-        crawler = crawler->children[index];
-        size_common++;
-    }
-    return size_common;
-    
-}
-void construct_tries(uint8_t strings[2][4000], int num, TrieNode *root, int stlen) {
-    uint8_t *s;
-
-    for (int i = 0; i < num; ++i) {
-        s = strings[i];
-        insert_trie(root, s, stlen);
-    }
-}
-int len_lcp(uint8_t strings[2][4000], int num, int out[], int stlen) {
-    TrieNode *root = get_node_trie();
-    construct_tries(strings, num, root, stlen);
-
-    int res = walk_trie(root);
-    free(root);
-
-    out[0] = res;
-    return res;    
-}
-
-
 void encode_gatacca(chartype_t in[SIZE_CHARS], outtype_t out[SIZE_OUT]) {
     __m256i all_threes = _mm256_set1_epi8(3);
     __m256i sl_nums = _mm256_setr_epi64x(0, 2, 4, 6);
@@ -243,11 +88,11 @@ int get_hamming_integers(hamtype_t a[SIZE_HAM], hamtype_t b[SIZE_HAM]) {
 }
 
 
-void get_hamming_cluster(hamtype_t in_cluster[][SIZE_HAM], size_t len_rows, int *out) {
-  //  hamtype_t (*in_2d)[len_rows][len_cols] = (hamtype_t (*)[len_rows][len_cols])in;
+void get_hamming_cluster(hamtype_t *in_cluster, size_t len_rows, int *out) {
+    hamtype_t (*in_2d)[len_rows][SIZE_HAM] = (hamtype_t (*)[len_rows][SIZE_HAM])in_cluster;
 
-    hamtype_t a[len_cols];
-    hamtype_t b[len_cols];
+    hamtype_t a[SIZE_HAM];
+    hamtype_t b[SIZE_HAM];
     int64_t i = -1;
     int64_t j = 0;
     int out_i = 0;
@@ -265,10 +110,10 @@ void get_hamming_cluster(hamtype_t in_cluster[][SIZE_HAM], size_t len_rows, int 
             out_j = out[j];
             if (out_j != -1) { j++; continue; }
   
-            memcpy(a, in_2d[i], len_cols * sizeof(hamtype_t));
-            memcpy(b, in_2d[j], len_cols * sizeof(hamtype_t));
+            memcpy(a, in_2d[i], SIZE_HAM * sizeof(hamtype_t));
+            memcpy(b, in_2d[j], SIZE_HAM * sizeof(hamtype_t));
 
-            diff = get_hamming_integers(a, b, len_cols); 
+            diff = get_hamming_integers(a, b); 
             if (diff < 2) {
                 out[j] = i;
                 goto set_j;                
@@ -280,17 +125,22 @@ void get_hamming_cluster(hamtype_t in_cluster[][SIZE_HAM], size_t len_rows, int 
 }
 
 
-uint32_t hash32(uint64_t x) {
-  uint32_t low = (uint32_t)x;
-  uint32_t high = (uint32_t)(x >> 32);
-  return (uint32_t)((A * low + B * high + C) >> 32);
+hmsize_t hash_bits(uint64_t x) {
+  hmsize_t low = (hmsize_t)x;
+  hmsize_t high = (hmsize_t)(x >> HM_SHIFT);
+  return (hmsize_t)((A * low + B * high + C) >> HM_SHIFT);
 }
 
-size_t hash_tuple_to_index(uint32_t x, size_t len) {
-    uint32_t hash_x = hash32(x);
-    uint32_t hash_len = hash32(len);
+hmsize_t next_round_bits(hmsize_t n) {
+    return ROUNDUP_32(n);
+}
 
-    return (size_t)((((hash_x ^ PYHASH_X) * PYHASH_MULT) ^ PYHASH_X) * (PYHASH_MULT + 1))
+
+hmsize_t hash_tuple_to_index(uint64_t x, hmsize_t len) {
+    hmsize_t hash_x = hash_bits(x);
+
+    hmsize_t ret = (hmsize_t)((((hash_x ^ PYHASH_X) % PYHASH_REM1) ^ (PYHASH_X ^ (len >> 2))) % (PYHASH_REM2));
+    return ret; 
 }
 
 hm_s *new_hashmap() {
@@ -301,82 +151,95 @@ hm_s *new_hashmap() {
         exit(139);
     }
 
-    hm->vec_vec = NULL;
+    hm->vec_vec = (hmvalue_s*)calloc(1, sizeof(hmvalue_s));
     hm->n = 0;
     hm->next_round = 0;
 
     return hm;
 
 }
-hmvalue_s *new_hmv(size_t len_seq) {
-    hmvalue_s *hmv = malloc(sizeof(hm_s));
+void init_hmv(hm_s *self, hmsize_t index, hmsize_t len_seq) {
+    self->vec_vec[index] = (hmvalue_s){.arr=malloc(sizeof(hm_node)), .len_seq=len_seq, .n=0};
+}
 
-    if (!hmv) {
-        printf("Error allocating hashmap on heap\n");
-        exit(139);
+void resize_insert_hmn(hmn_t self, hmsize_t index, seq_t seq, hmsize_t len) {
+    self[index - 1] = (hm_node){.seq = malloc(sizeof(uint64_t) * len), .len_seq=len};
+
+    for (hmsize_t i = 0; i < len; ++i) {
+        self[index - 1].seq[i] = seq[i];
     }
+}
 
-    hmv->arr = NULL;
-    hmv->len_seq = len_seq;
-    hmv->n = 0;
-    hmv->next_round = 0;
-
-    return hmv;
+void resize_insert_hmv(hmvalue_s *self, seq_t seq) {
+    self->n++;
+    self->arr = (hmn_t)realloc(self->arr, sizeof(hm_node) * self->n);
+    resize_insert_hmn(self->arr, self->n, seq, self->len_seq);
 }
 
 void resize_hashmap(hm_s *self) {
-    self->next_round = ROUNDUP_32(self->n);
+    hmsize_t rounded_up = self->next_round = next_round_bits(self->n);
 
-    if (self->next_round > self->n) {
-        self->vec_vec = (hmvalue_s**)realloc(self->vec_vec, self->next_round * sizeof(hmvalue_s*));
-
-        if (!self->vec_vec) {
-            printf("Error reallocating hashmap value vector on heap to %lu\n", self->next_round);
+    if (rounded_up > self->n) {
+        hmvalue_s *p_new = (hmvalue_s*)realloc(self->vec_vec, rounded_up * sizeof(hmvalue_s));
+        if (!p_new) {
+            printf("Error reallocating hashmap value vector on heap to %hu\n", self->next_round);
             exit(139);
         }
+        self->vec_vec = p_new;
     }
 }
 
-void resize_hashmap_vec(hmvalue_s *self) {
-    self->next_round = ROUNDUP_32(self->n);
 
-    if (self->next_round > self->n) {
-        self->arr = (uint64_t*)realloc(self->arr, self->next_round * sizeof(uint64_t) * self->len_seq);
-
-        if (!self->arr) {
-            printf("Error reallocating sequence holder array on heap to %lu\n", self->next_round);
-            exit(139);
-        }
+void free_hashmap_vec(hmvalue_s self) {
+    for (int i = 0; i < self.n; i++) {
+        free(self.arr[i].seq);
     }
-}
-
-void free_hashmap_vec(hmvalue_s *self) {
-    free(self);
 }
 
 void free_hashmap(hm_s *self) {
-    for (size_t i = 0; i < self->n; ++i) free_hashmap_vec(self->vec_vec[i]);
+    for (hmsize_t i = 0; i < self->n; ++i) free_hashmap_vec(self->vec_vec[i]);
 
     free(self);
 }
 
-void insert_into_hashmap(hm_s *self, uint64_t key, uint64_t *value, size_t len_seq) {
-    size_t vec_index = hash_tuple_to_index(key, len_seq);
+void insert_into_hashmap(hm_s *self, uint64_t key, seq_t  value, hmsize_t len_seq) {
+    hmsize_t vec_index = hash_tuple_to_index(key, len_seq);
 
     if (self->n < vec_index) {
         self->n = vec_index;
         resize_hashmap(self);
 
-        self->vec_vec[vec_index - 1] = new_hmv();
+        init_hmv(self, vec_index - 1, len_seq);
     }
 
-    self->vec_vec[vec_index - 1]->n += 1;
-    resize_hashmap_vec(self->vec_vec[vec_index - 1]);
-    for (int i = 0; i < len_seq; i++) self->vec_vec[vec_index - 1]->arr[i + self->vec_vec[vec_index - 1]->n - 1] = value[i];
+    resize_insert_hmv(&self->vec_vec[vec_index - 1], value);
 }
 
-hmvalue_s *get_hashmap_value(hm_s *self, uint64_t key, size_t len_seq) {
+hmvalue_s get_hashmap_value(hm_s *self, uint64_t key, hmsize_t len_seq) {
     size_t vec_index = hash_tuple_to_index(key, len_seq);
 
     return self->vec_vec[vec_index - 1];
+}
+
+void print_hmv(hmvalue_s hmv) {
+    for (int i = 0; i < hmv.n; i++) print_hmn(hmv.arr[i]);
+}
+void print_hmn(hm_node hmn) {
+    for (int i = 0; i < hmn.len_seq; i++) printf("%lu/%lx, ", hmn.seq[i], hmn.seq[i]);
+
+    printf("\n------\n");
+}
+
+int main() {
+    hm_s *hm = new_hashmap();
+
+    uint64_t key = 0x010101ddeeff1142;
+    uint64_t seq[] = {0x010101ddeeff1142, 0xf10101ddeeff1e42, 0x010101ddeefe1143,  0x01e101fdefff1142};
+
+    insert_into_hashmap(hm, key, seq, 4);
+
+    hmvalue_s v = get_hashmap_value(hm, key, 4);
+
+    print_hmv(v);
+
 }
--- fastcompare.h
+++ fastcompare.h
@@ -14,122 +14,68 @@
 #define SIZE_HAM 4
 
 #define PYHASH_X 0x345678
-#define PYHASH_MULT 1000003
+#define PYHASH_REM2 0xfffffffb
+#define PYHASH_REM1 0xffffffef
 
-#define A 0x02502ef84dbecade
-#define B 0x27112ee84dfecafa
-#define C 0x191022f84ddecadb
+#define A 0x000123fdea49fedc
+#define B 0x930233fdaa39ffdd
+#define C 0x112309df9edf91df
 
 
 
 #define ROUNDUP_32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
 
-#define kvec_t(type) struct { size_t n, m; type *a; }
-#define kv_init(v) ((v).n = (v).m = 0, (v).a = 0)
-#define kv_destroy(v) free((v).a)
-#define kv_A(v, i) ((v).a[(i)])
-#define kv_pop(v) ((v).a[--(v).n])
-#define kv_size(v) ((v).n)
-#define kv_max(v) ((v).m)
-
-#define kv_resize(type, v, s) do { \
-		if ((v).m < (s)) { \
-			(v).m = (s); \
-			kv_roundup32((v).m); \
-			(v).a = (type*)realloc((v).a, sizeof(type) * (v).m); \
-		} \
-	} while (0)
-
-#define kv_copy(type, v1, v0) do {							\
-		if ((v1).m < (v0).n) kv_resize(type, v1, (v0).n);	\
-		(v1).n = (v0).n;									\
-		memcpy((v1).a, (v0).a, sizeof(type) * (v0).n);		\
-	} while (0)												\
-
-#define kv_push(type, v, x) do {									\
-		if ((v).n == (v).m) {										\
-			(v).m = (v).m? (v).m<<1 : 2;							\
-			(v).a = (type*)realloc((v).a, sizeof(type) * (v).m);	\
-		}															\
-		(v).a[(v).n++] = (x);										\
-	} while (0)
-
-#define kv_a(type, v, i) ((v).m <= (size_t)(i)?						\
-						  ((v).m = (v).n = (i) + 1, kv_roundup32((v).m), \
-						   (v).a = (type*)realloc((v).a, sizeof(type) * (v).m), 0) \
-						  : (v).n <= (size_t)(i)? (v).n = (i)			\
-						  : 0), (v).a[(i)]
-
-#define kv_reverse(type, v, start) do { \
-		if ((v).m > 0 && (v).n > (start)) { \
-			size_t __i, __end = (v).n - (start); \
-			type *__a = (v).a + (start); \
-			for (__i = 0; __i < __end>>1; ++__i) { \
-				type __t = __a[__end - 1 - __i]; \
-				__a[__end - 1 - __i] = __a[__i]; __a[__i] = __t; \
-			} \
-		} \
-	} while (0)
-
-
-
 typedef uint8_t chartype_t;
 typedef uint64_t outtype_t;
 typedef uint64_t hamtype_t;
 
-typedef struct TSTNode {
-    uint8_t data;
-    struct TSTNode *left, *right, *eq;
-    int is_end;
-} TSTNode;
-TSTNode *new_node(uint8_t data);
-int search_tst(TSTNode *root, uint8_t *word);
-void insert_tst(TSTNode **root, uint8_t *word);
-void _traverse_tst(TSTNode *root, uint8_t *buffer, int depth);
-uint8_t *traverse_tst(TSTNode *root);
-typedef struct TrieNode {
-    struct TrieNode *children[ALPHA_SIZE];
-    int is_end;
-} TrieNode;
-
-TrieNode *get_node_trie();
-void insert_trie(TrieNode *root, uint8_t *key, int stn);
-int count_children_trie(TrieNode *root, int *index);
-int walk_trie(TrieNode *root);
-void construct_tries(uint8_t strings[2][4000], int num, TrieNode *root, int stn);
-int len_lcp(uint8_t strings[2][4000], int num, int out[], int stn);
-int find_common_prefix(TSTNode *root, uint8_t *str, int *uint8_tindex);
-
 int get_hamming_integers(hamtype_t a[SIZE_HAM], hamtype_t b[SIZE_HAM]);
-void get_hamming_cluster(hamtype_t in_cluster[][SIZE_HAM], size_t len_rows, int *out);
+void get_hamming_cluster(hamtype_t *in_cluster, size_t len_rows, int *out);
 void reduce_integer_or_op(outtype_t in, outtype_t *reducer);
 void encode_gatacca(chartype_t in[SIZE_CHARS], outtype_t out[SIZE_OUT]);
 void pack_32_bytes_in_64_bits(chartype_t in[SIZE_CHARS], outtype_t result[1]);
 
-uint32_t hash32(uint64_t x);
-size_t hash_tuple_to_index(uint32_t x, size_t len);
-int next_round_32(int *in);
+#define SZ_MAX 256
 
-#define SIZE_MAX 256
+typedef uint64_t* seq_t;
+typedef uint32_t hmsize_t;
+
+hmsize_t hash_bits(uint64_t x);
+hmsize_t hash_tuple_to_index(uint64_t x, hmsize_t len);
+hmsize_t next_round_bits(hmsize_t n);
+
+#define HM_SHIFT 32
+
+typedef struct HashMapNode {
+    seq_t seq;
+    uint16_t len_seq;
+} hm_node;
+
+typedef hm_node* hmn_t;
 
 typedef struct HashMapValue {
-    uint64_t *arr;
-    size_t len_seq;
-    size_t n;
-    size_t next_round;
+    hmn_t arr;
+    hmsize_t len_seq;
+    hmsize_t n;
 } hmvalue_s;
 
+typedef hmvalue_s* hmv_t;
+
 typedef struct HashMapSt {
-    hmvalue_s** vec_vec;
-    size_t n;
-    size_t next_round;
+    hmv_t vec_vec;
+    hmsize_t n;
+    hmsize_t next_round;
 } hm_s;
 
 hm_s *new_hashmap();
-hmvalue_s *new_hmv();
+void init_hmv(hm_s *self, hmsize_t index, hmsize_t len_seq);
+void init_hmn(hmn_t *self, hmsize_t index, seq_t seq, hmsize_t len);
+void resize_insert_hmv(hmvalue_s *self, uint64_t *seq);
 void resize_hashmap(hm_s *self);
-void resize_hashmap_vec(hmvalue_s *self);
-void free_hashmap_vec(hmvalue_s *self);
+void free_hashmap_vec(hmvalue_s self);
 void free_hashmap(hm_s *self);
-void insert_into_hashmap(hm_s *self, uint64_t key, uint64_t *value, size_t len_seq);
-hmvalue_s *get_hashmap_value(hm_s *self, uint64_t key, size_t len_seq);
+void insert_into_hashmap(hm_s *self, uint64_t key, uint64_t *value, hmsize_t len_seq);
+hmvalue_s get_hashmap_value(hm_s *self, uint64_t key, hmsize_t len_seq);
+
+void print_hmv(hmvalue_s hmv);
+void print_hmn(hm_node hmn);
